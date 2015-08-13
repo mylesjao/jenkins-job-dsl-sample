6e5ee6289a55a36b559f70dd0008a9331e12ee8b
mylesjao <mrlau1@gmail.com>
2015-08-14 00:53:12 +0800

    show current working dir

--- build/dist.sh
+++ build/dist.sh
@@ -2,6 +2,8 @@
 PROJECT_HOME="$(cd "`dirname "$0"`"/..; pwd)"
 cd ${PROJECT_HOME}
 
+pwd
+
 VERSION=` grep "version" version.sbt |awk -F":=" '{ print $2 }' |sed -e 's/^[[:space:]]*//'|tr -d '"' `
 
 # docker build
