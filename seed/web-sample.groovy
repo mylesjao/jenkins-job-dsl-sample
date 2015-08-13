b284aa9d53c16b9121aac248e1d6cf7fb7f42ada
mylesjao <mrlau1@gmail.com>
2015-08-13 18:45:41 +0800

    add basic job

---
+++ seed/web-sample.groovy
@@ -0,0 +1,34 @@
+freeStyleJob("build") {
+	
+	description "A job to compile, unit-test, package and make distributed tarball"
+	
+	scm {
+		git("https://github.com/mylesjao/jenkins-job-dsl-sample.git", "master")
+	}
+
+	triggers {
+		scm('*/15 * * * *')
+	}
+
+	steps {
+		shell("build/build.sh")
+	}
+
+}
+
+freeStyleJob("dist") {
+
+	description "A to make a docker image"
+}
+
+/*freeStyleJob("publish") {
+
+	description "A job to publish docker image to registry"
+	
+}
+
+freeStyleJob("deploy") {
+
+	description "A job to pull docker from registry and run"
+	
+}*/
