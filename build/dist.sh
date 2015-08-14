114b6103c11ea614095f7e80d37ada3fbcbd5955
mylesjao <mrlau1@gmail.com>
2015-08-14 15:36:12 +0800

    move tag function to publish.sh

--- build/dist.sh
+++ build/dist.sh
@@ -6,15 +6,16 @@ pwd
 
 VERSION=` grep "version" version.sbt |awk -F":=" '{ print $2 }' |sed -e 's/^[[:space:]]*//'|tr -d '"' `
 
-REGISTRY_HOST=localhost:5000
-
 # docker build
 ASSEMBLY="${PROJECT_HOME}/dist/web-sample.tar.gz"
 if [ -f "${ASSEMBLY}" ]
 then
+	echo "remove previous images..."
+	docker rmi -f web-sample:${VERSION}
+
 	echo "build docker images..."
 	cp build/Dockerfile dist/
-	docker build -t ${REGISTRY_HOST}/web-sample:${VERSION} dist/
+	docker build -t web-sample:${VERSION} dist/
 else
 	echo "${ASSEMBLY} not found. Please run build.sh first"
 fi
--- build/publish.sh
+++ build/publish.sh
@@ -8,5 +8,6 @@ VERSION=` grep "version" version.sbt |awk -F":=" '{ print $2 }' |sed -e 's/^[[:s
 # change this to private registry host if need
 REGISTRY_HOST=localhost:5000
 
-# push to docker hub
+# push to docker registry
+docker tag web-sample:${VERSION} ${REGISTRY_HOST}/web-sample:${VERSION}
 docker push ${REGISTRY_HOST}/web-sample:${VERSION}
